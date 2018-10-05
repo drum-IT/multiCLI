1f4bdaac9f30ca32cfc81d6a98ae5ce5a26a0543
Miles <milesdpratt@gmail.com>
2018-10-04 20:06:09 -0400

    Add clear, figlet, and chalk packages for clearing the console, printing ascii art text, and coloring the output. Updated clock command to use these packages.

--- cmds/clock.js
+++ cmds/clock.js
@@ -1,6 +1,9 @@
 // get dependencies
 const ora = require("ora");
 const moment = require("moment-timezone");
+const clear = require("clear");
+const figlet = require("figlet");
+const chalk = require("chalk");
 const getLocation = require("../utils/location");
 
 // get the current weather conditions for a location.
@@ -13,9 +16,24 @@ module.exports = async args => {
       .tz(locationData.timezone)
       .format("MMMM Do YYYY, h:mm:ss a");
     spinner.stop();
+
+    clear();
+    console.log();
     console.log(
-      `Current time in ${locationData.city}, ${locationData.region}: ${time}`
+      chalk.yellow(
+        figlet.textSync("Clock", {
+          font: "bulbhead",
+          horizontalLayout: "full"
+        })
+      )
     );
+    console.log();
+    console.log(
+      chalk.yellow(
+        `Current time in ${locationData.city}, ${locationData.region}: ${time}`
+      )
+    );
+    console.log();
   } catch (err) {
     spinner.stop();
     console.error(err);

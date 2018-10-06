ca11e6c95493993cfa79719cb8009bd3a7370130
Miles <milesdpratt@gmail.com>
2018-10-06 19:01:12 -0400

    Update help text for image command.

--- cmds/help.js
+++ cmds/help.js
@@ -13,6 +13,7 @@ const menus = {
     clock .............. get date and time for current location based on IP address
     country............. get helpful country information for a location
     quote .............. display a random quote
+    image .............. get a random url image of specified dimenions from picsum API
     
     `,
   weather: `
